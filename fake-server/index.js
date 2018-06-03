cf84c564506d970bcfbbf7a0a042f7b7e03e3ef5
Joseph Khan <jsphkhan@gmail.com>
2018-06-03 16:17:21 +0530

    created a node interface for fake json-server
    This helped me in deploying it to Now server

---
+++ fake-server/index.js
@@ -0,0 +1,18 @@
+//https://github.com/nbkhope/fake-restful-api
+
+const { exec } = require('child_process');
+
+// Set port (default: 3000). For Heroku, we need to use
+// the port set by the environment variable $PORT
+const port = process.env.PORT || 3002;
+
+const command = `json-server --watch db.json --port ${port}`;
+
+exec(command, (err, stdout, stderr) => {
+  if (err) {
+    console.log('Error running exec', err);
+    return;
+  }
+  console.log('stdout:', stdout);
+  console.log('stderr:', stderr);
+});
